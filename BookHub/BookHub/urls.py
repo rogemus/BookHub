befe205ba5f838d237fd891cef67a24fa2cd27d1
Maciej Nowakowski <macnow@st.amu.edu.pl>
2018-03-15 20:54:43 +0100

    BOOK-26 | Create Comments App

--- BookHub/BookHub/urls.py
+++ BookHub/BookHub/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework_nested import routers as nested_routers
 
 from books import views
 from books.views import BookHubApi
+from comments.views import CommentViewSet
 
 
 class DocumentedRouter(routers.DefaultRouter):
@@ -15,7 +17,11 @@ api_router.register(r'books', views.BookViewSet)
 api_router.register(r'authors', views.AuthorViewSet)
 api_router.register(r'publishers', views.PublisherViewSet)
 
+domains_router = nested_routers.NestedSimpleRouter(api_router, r'books', lookup='books')
+domains_router.register(r'comments', CommentViewSet, base_name='book-comments')
+
 urlpatterns = [
     path('api/', include(api_router.urls)),
+    path('api/', include(domains_router.urls)),
     path('admin/', admin.site.urls),
 ]
---
+++ BookHub/comments/views.py
@@ -0,0 +1,8 @@
+from rest_framework import viewsets
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows books to be viewed or edited.
+    """
+    pass

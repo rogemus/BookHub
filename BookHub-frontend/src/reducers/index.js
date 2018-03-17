569b5dd65d65c78779e532b3ba4ea6c53c50dbbc
Kacper <rogemus@gmail.com>
2018-03-17 14:50:15 +0100

    remove redundant reducers

--- BookHub-frontend/src/reducers/index.js
+++ BookHub-frontend/src/reducers/index.js
@@ -2,11 +2,9 @@ import {
 	combineReducers
 } from 'redux';
 
-import ActiveBookReducers from './activeBook.reducer';
 import BookReducers from './book.reducer';
 
 const rootReducer = combineReducers({
-	activeBook: ActiveBookReducers,
 	book: BookReducers
 });
 

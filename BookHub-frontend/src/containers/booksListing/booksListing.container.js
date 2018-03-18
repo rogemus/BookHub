660e7c074e49972f3519404d9c13e8df0dddd1d1
Kacper <rogemus@gmail.com>
2018-03-18 08:53:45 +0100

    change file name

--- BookHub-frontend/src/containers/booksListing/booksListing.container.js
+++ BookHub-frontend/src/containers/booksListing/booksListing.container.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {getBooks} from '../../actions/books.actions';
 import isEmpty from 'lodash/isEmpty';
-import './listing.css';
+import './booksListing.styles.css';
 import ListingItem from '../../components/listingItem/listingItem.component';
 
 class BookListing extends Component {

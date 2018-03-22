55da3410d88113c64c1494650874f28438c8b745
Xaroun <mateusz1003@gmail.com>
2018-03-22 22:29:24 +0100

    BOOK-30 | Utworzenie modelu Usera, poprawki

--- BookHub/users/views.py
+++ BookHub/users/views.py
@@ -1,29 +1,16 @@
-from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework import viewsets, mixins
+from rest_framework.permissions import AllowAny
+from rest_framework.viewsets import GenericViewSet
+
 from users.models import User
 from users.serializers import UserSerializer
 
 
-class UserViewSet(viewsets.ModelViewSet):
+class UserViewSet(mixins.CreateModelMixin, mixins.ListModelMixin, mixins.RetrieveModelMixin, GenericViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
-    queryset = User.objects.all().order_by('-username')
-    permission_classes = (IsAuthenticatedOrReadOnly,)
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
     serializer_class = UserSerializer
     filter_fields = ('email', 'username',)
-
-
-# @api_view(['GET', 'POST'])
-# def register(request):
-#     """
-#     Register a new user
-#     """
-#     if request.method == 'POST':
-#         serializer = UserSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-#     elif request.method == 'GET':
-#         return Response(status=status.HTTP_403_FORBIDDEN)

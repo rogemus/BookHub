078d8ee58848573ddc9dbfea22c4278882b8ca17
Xaroun <mateusz1003@gmail.com>
2018-03-22 23:26:39 +0100

    BOOK-31 | Endpoint API /users/

--- BookHub/users/models.py
+++ BookHub/users/models.py
@@ -6,13 +6,7 @@ class User(models.Model):
     username_validator = UnicodeUsernameValidator()
     email = models.EmailField(blank=False)
 
-    username = models.CharField(max_length=30, unique=True,
-                                help_text='Required. 30 characters or fewer. Letters, digits and ''@/./+/-/_ only.',
-                                validators=[username_validator],
-                                error_messages={
-                                    'unique': "A user with that username already exists.",
-                                })
-
+    username = models.CharField(max_length=30, unique=True, validators=[username_validator],)
     password = models.CharField(max_length=64)
 
     def __str__(self):

2fc1d1814e1dd3db4cdd884ddea91f0830370458
Xaroun <mateusz1003@gmail.com>
2018-03-25 15:54:09 +0200

    BOOK-31 | Endpoint API /users/, tests+deletion

--- BookHub/users/views.py
+++ BookHub/users/views.py
@@ -9,9 +9,10 @@ from users.serializers import UserSerializer
 class CreateListRetrieveViewSet(mixins.CreateModelMixin,
                                 mixins.ListModelMixin,
                                 mixins.RetrieveModelMixin,
+                                mixins.DestroyModelMixin,
                                 viewsets.GenericViewSet):
     """
-    A viewset that provides `retrieve`, `create`, and `list` actions.
+    A viewset that provides `retrieve`, `create`, `delete` and `list` actions.
 
     To use it, override the class and set the `.queryset` and
     `.serializer_class` attributes.

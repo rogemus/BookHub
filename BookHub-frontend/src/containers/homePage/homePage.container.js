c12838d52ec3823c6c93929288d1adefa6771982
Kacper Rogowski <kacper.rogowski@cognifide.com>
2018-04-13 23:11:03 +0200

    refactor

--- BookHub-frontend/src/containers/homePage/homePage.container.js
+++ BookHub-frontend/src/containers/homePage/homePage.container.js
@@ -1,13 +1,12 @@
-import React, {Component} from 'react';
-import BooksListing from '../booksListing/booksListing.container';
-import RegisterPage from '../registerPage/registerPage.container';
+import React, { Component } from 'react';
+import Listing from '../listing/listing.container';
 
 class HomePage extends Component {
 	render() {
 		return (
 			<div>
 				<h1>Home</h1>
-				<BooksListing/>
+				<Listing />
 			</div>
 		);
 	}
